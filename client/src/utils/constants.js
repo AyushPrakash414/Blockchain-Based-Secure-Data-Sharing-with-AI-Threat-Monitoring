73722a6b7f8d42dfb9fa4d2247294e71fffed8ac
Ayush <prakashayush414@gmail.com>
2026-04-17 12:04:02 +0530

    Add client/src/utils/constants.js

---
+++ client/src/utils/constants.js
@@ -0,0 +1,18 @@
+
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                 Smart Contract                                 ||
+// ! ||--------------------------------------------------------------------------------||
+
+import abi from './upload.json'
+export const contractAddress = '0x4525DF2f6c6Dddf4A5c1Fe2Fa53E40299cD311C8';
+export const contractAbi = abi;
+
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                   Pinata IPFS                                  ||
+// ! ||--------------------------------------------------------------------------------||
+
+export const API_Key = '3f008041436a703ad4a2'
+export const API_Secret = 'a3afe2b97fe1af1647e8a5e73867a6ca4842f20a6218bcb8e063d9496aa1649c'
+export const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxN2ViMmE2OC03ZGM0LTRmOTUtODVhZi05ZjhlNmYxYzE2NjkiLCJlbWFpbCI6InNhcnZhZ3lhNjUzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIzZjAwODA0MTQzNmE3MDNhZDRhMiIsInNjb3BlZEtleVNlY3JldCI6ImEzYWZlMmI5N2ZlMWFmMTY0N2U4YTVlNzM4NjdhNmNhNDg0MmYyMGE2MjE4YmNiOGUwNjNkOTQ5NmFhMTY0OWMiLCJleHAiOjE4MDc4ODA1NjN9.tM9FRhnvAJwbFmaIRV92pgcQd1NYQh-qe0bxfcEdb9A'
