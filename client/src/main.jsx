1ee45480348ea8370ad5b27b3d2316308a8542ab
Ayush <prakashayush414@gmail.com>
2026-04-18 05:19:59 +0530

    Testing f

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,16 @@ import './index.css'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      registrations.forEach(registration => {
+        registration.unregister()
+      })
+    })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
