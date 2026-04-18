69fd583262220994e84633e1382a311a75da6ddd
Ayush <prakashayush414@gmail.com>
2026-04-18 05:36:09 +0530

    Adding config test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,16 +7,6 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 import { WalletProvider } from './context/WalletContext.jsx'
 import App from './App.jsx'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      registrations.forEach(registration => {
-        registration.unregister()
-      })
-    })
-  })
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
